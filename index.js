ea829cd27f9b7123fd8b05274003027b469bcee8
Lucas Schlierf <lucas.schlierf@tum.de>
2023-08-28 14:25:05 +0200

    added sevenheaven frontend

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+const app = express();//serve up production assets
+app.use(express.static('sevenheaven-site/build'));
+// let the react app to handle any unknown routes 
+// serve up the index.html if express does'nt recognize the route
+import * as path from 'path'
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'sevenheaven-site', 'build', 'index.html'));
+});
+// if not in production use the port 5000
+const PORT = process.env.PORT || 5000;
+console.log('server started on port:',PORT);
+app.listen(PORT);
