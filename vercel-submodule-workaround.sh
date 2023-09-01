6209dbe7a2a297e169fc0a20f0b879e9b1a5cf48
Lucas Schlierf <lucas.schlierf@tum.de>
2023-09-01 16:32:41 +0200

    edited vercel script

--- vercel-submodule-workaround.sh
+++ vercel-submodule-workaround.sh
@@ -1,8 +1,8 @@
 # github submodule repo address without https:// prefix
-SUBMODULE_GITHUB=github.com/beeinger/vercel-private-submodule
+SUBMODULE_GITHUB=github.com/lschlierf/sevenheaven-site
 
 # .gitmodules submodule path
-SUBMODULE_PATH=library
+SUBMODULE_PATH=sevenheaven-site
 
 # github access token is necessary
 # add it to Environment Variables on Vercel
