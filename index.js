79aaebb1a882ad65fbc4ac0d5ec87822c8013ff4
Lucas Schlierf <LucasSchlierf@gmail.com>
2023-09-16 20:57:50 -0700

    body parse json

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import express from 'express';
 const app = express();
+import bodyParser from 'body-parser';
 app.use(express.static('sevenheaven-site/build'));
-app.use(express.json())
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 import * as path from 'path';
 import { fileURLToPath } from 'url';
@@ -9,11 +11,15 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// function getCredentials() {
+    
+// }
+
 app.post('/api', (req, res) => {
     console.log('got post request to /api from ' + req.ip);
     console.log(req.body);
 
-    //handle malformed request errors
+    // handle malformed request errors
     const request = req.body
     if (!request) {
         res.json({ 'status': 'error', 'error': 'missing request body' })
@@ -32,9 +38,12 @@ app.post('/api', (req, res) => {
     // }, 2000)
 })
 
-app.get('*', (_, res) => {
-    res.sendFile(path.resolve(__dirname, 'sevenheaven-site', 'build', 'index.html'));
-});
+// serve frontend only if not in production
+if (!process.env.PORT) {
+    app.get('*', (_, res) => {
+        res.sendFile(path.resolve(__dirname, 'sevenheaven-site', 'build', 'index.html'));
+    });
+}
 
 // if not in production use the port 5000
 const PORT = process.env.PORT || 5000;
